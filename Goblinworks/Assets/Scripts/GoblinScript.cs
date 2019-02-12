60b6aa816fc4c08d712718d9e3fad1e8c99dd83a
Alex <alexanderwest0598@gmail.com>
2019-02-12 14:36:33 -0500

    Added mining post, started on goblin task functionality

--- Goblinworks/Assets/Scripts/GoblinScript.cs
+++ Goblinworks/Assets/Scripts/GoblinScript.cs
@@ -16,15 +16,102 @@ public class GoblinScript : MonoBehaviour
 	        * Will probably be spell based in the end. 
 	    * Carry weight.
      */
+
+    // Number of runes/rock the goblin can carry at any point and time
+    [SerializeField]
+    int runeCarryWeight = 1;
+    int numRunes = 0;
+    // Time it takes for the goblin to mine a rock
+    [SerializeField]
+    float mineTime = .5f;
+    // How many runes/rocks the gobling mines at once
+    [SerializeField]
+    int miningPower = 1;
+    float miningTimer = 0.0f;
+    // Movespeed of the goblin
+    [SerializeField]
+    float moveSpeed = 1.0f;
+
+    // current task the goblin is assigned
+    int currentTask = -1;
+    enum Tasks
+    {
+        INVALID_TASK = -1,
+        MINE_TASK = 0,
+        RUNNER_TASK = 1,
+    }
+
+    GameObject baseObject;
+    GameObject miningPost;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        baseObject = GameObject.Find("Base");
+        miningPost = GameObject.Find("Mining Post");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch(currentTask)
+        {
+            case (int)Tasks.MINE_TASK:
+            {
+                MineTask();
+                break;
+            }
+            case (int)Tasks.RUNNER_TASK:
+            {
+                RunnerTask();
+                break;
+            }
+            default:
+                break;
+        }
+    }
+
+    void MineTask()
+    {
+        if (runeCarryWeight == numRunes)
+        {
+            Debug.Log("Going To Base");
+            Vector3.MoveTowards(transform.position, baseObject.transform.position, moveSpeed);
+        }
+        else if(numRunes == 0 && transform.position != miningPost.transform.position)
+        {
+            Debug.Log("Going To Mine");
+            Vector3.MoveTowards(transform.position, miningPost.transform.position, moveSpeed);
+        }
+        else
+        {
+            Debug.Log("Mining");
+            miningTimer += Time.deltaTime;
+            if (miningTimer >= mineTime)
+            {
+                numRunes += miningPower;
+                if (numRunes >= runeCarryWeight)
+                {
+                    numRunes = runeCarryWeight;
+                }
+                miningTimer = 0.0f;
+            }
+        }
+    }
+
+    void RunnerTask()
+    {
+
+    }
+
+    // set task by name
+    public void SetTask(string taskName)
+    {
+    }
+
+    // set task by index
+    public void SetTask(int taskNum)
+    {
+        currentTask = taskNum;
     }
 }
