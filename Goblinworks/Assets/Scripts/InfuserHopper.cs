6c82d0b43a15a158d42bd4dcd42db9041da1a627
Jacob <jacob.biederman@mymail.champlain.edu>
2019-02-12 20:51:14 -0500

    Added infuser hopper class

---
+++ Goblinworks/Assets/Scripts/InfuserHopper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfuserHopper : RuneHopper
+{
+    Queue<Rune> outGoingRunes;
+    [SerializeField]
+    int maxNumOutgoingRunes = 50;
+    // Start is called before the first frame update
+    void Start()
+    {
+        outGoingRunes = new Queue<Rune>();
+    }
+
+    public Rune GetRune()
+    {
+        if (outGoingRunes.Count <= 0)
+            return null;
+        return outGoingRunes.Dequeue();
+    }
+    public int getNumOutRunes()
+    {
+        return outGoingRunes.Count;
+    }
+    public int getMaxOutRunes()
+    {
+        return maxNumOutgoingRunes;
+    }
+}
